4e42f9b1518a9dad2a29b6eb1efdec3295805b8a
Plenglin <maximumpwnguin@gmail.com>
2015-12-04 17:34:15 -0800

    Updated PID

--- PID.java
+++ PID.java
@@ -11,7 +11,7 @@ class PidDrive {
 	}
 
 	public void moveForward(Queue queue, double power, long time) {
-		queue.add(new PidDriveAction(this, power, time));
+		queue.shift(new PidDriveAction(this, power, time));
 	}
 
 	public void setBothPowers(double power) {
@@ -23,15 +23,15 @@ class PidDrive {
 
 class PidDriveAction extends Routine {
 
-	public static final double THRESHOLD = 0.05;
-	public static final long DELTA_TIME = 100d;
-
-	double adjustment = -2;
+	public static final double THRESHOLD = 0.05d;
+	public static final long DELTA_TIME = 100l;
 
 	DcMotor slave, master;
 	SpeedChecker slaveSpeed, masterSpeed;
 	double power;
 	long end;
+
+	public static final double sensitivity = 0.1d;
 
 	public PidDriveAction(PidDrive drive, double power, long time) {
 		
@@ -64,9 +64,9 @@ class PidDriveAction extends Routine {
 		masterSpeed.update();
 		double vSlave = slaveSpeed.getSpeed(), vMaster = masterSpeed.getSpeed();
 		boolean speedWithinThreshold = vMaster*(1-THRESHOLD) < vSlave && vSlave < vMaster*(1+THRESHOLD);
-		boolean overshot = (adjustment > 0 && vSlave > vMaster) || (adjustment < 0 && vSlave < vMaster);
-		if (!speedWithinThreshold && overshot) {
-			adjustment *= -0.25;
+		if (!speedWithinThreshold) {
+			double adjustment = 1 - (vMaster/vSlave);
+			slave.setPower(slave.getPower() + adjustment*sensitivity*DELTA_TIME);
 		}
 		return isFinished();
 	}
@@ -96,7 +96,7 @@ class SpeedChecker {
 	}
 
 	public double getSpeed() {
-		double currentPos = motor.getCurrentPosition();
+		int currentPos = motor.getCurrentPosition();
 		long currentTime = System.currentTimeMillis();
 		double speed = (currentPos-lastPos)/(currentTime-lastTime)/1000;
 		return speed;
